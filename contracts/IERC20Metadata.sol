e40b1049adec5e355ab1721a7d6cfd47c8ab5626
slowisfast2030 <slowisfast2030@163.com>
2023-03-02 17:31:22 +0800

    all is well

--- contracts/IERC20Metadata.sol
+++ contracts/IERC20Metadata.sol
@@ -10,3 +10,13 @@ interface IERC20Metadata is IERC20 {
     function symbol() external view returns (string memory);
     function decimals() external view returns (uint8);
 }
+
+/**
+Base functions can be overridden by inheriting contracts to change their behavior 
+if they are marked as virtual. The overriding function must then use the override keyword in the function header. 
+The overriding function may only change the visibility of the overridden function from external to public. 
+The mutability may be changed to a more strict one following the order: 
+nonpayable can be overridden by view and pure. 
+view can be overridden by pure. 
+payable is an exception and cannot be changed to any other mutability.
+*/
